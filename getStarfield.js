f00e518bc0f88370c3f0159d369c3ba7ba093195
tomas-lonnblad <tomaslonnblad25@gmail.com>
2025-01-16 16:43:43 +0100

    Update getStarfield.js

--- getStarfield.js
+++ getStarfield.js
@@ -36,7 +36,7 @@ export default function getStarfield({ numStars = 500 } = {}) {
     size: 0.2,
     vertexColors: true,
     map: new THREE.TextureLoader().load(
-      "./textures/stars/circle.png"
+      "circle.png"
     ),
   });
   const points = new THREE.Points(geo, mat);
